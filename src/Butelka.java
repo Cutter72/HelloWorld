e288770d09f558ab11f82326f4056e6013e21e1a
Cutter72 <drelich_pawel@o2.pl>
2018-11-20 16:52:29 +0100

    Do Butelki dodano właściwość - napełnienie

--- src/Butelka.java
+++ src/Butelka.java
@@ -1,11 +1,11 @@
-import java.sql.SQLOutput;
-
 public class Butelka
 {
     double pojemnosc;
+    double napelnienie;
 
-    Butelka(double zadanaPojemnosc)
+    Butelka(double zadanaPojemnosc, double zadaneNapelnienie)
     {
         this.pojemnosc=zadanaPojemnosc;
+        this.napelnienie=zadaneNapelnienie;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
-        Butelka butelka1 = new Butelka(1.5);
-        System.out.println(butelka1.pojemnosc);
+        Butelka butelka1 = new Butelka(1.5, 0.5);
+        System.out.println("Pojemnosc: " + butelka1.pojemnosc);
+        System.out.println("Napełnienie: " + butelka1.napelnienie);
     }
 }
