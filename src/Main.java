88c87fe9190b33168be179578d48e7decca4291b
Cutter72 <drelich_pawel@o2.pl>
2018-11-20 16:34:33 +0100

    Stworzenie klasy Butelka

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        Butelka butelka1 = new Butelka(1.5);
+        System.out.println(butelka1.pojemnosc);
     }
 }
